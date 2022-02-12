d092168171ce3db21c0adca38f4ee2b0f28fac74
alimmondal <76857187+alimmondal@users.noreply.github.com>
2022-02-12 15:53:03 +0600

    updated

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { Store } from '../utils/Store';
 
 const Shipping = () => {
   const router = useRouter();
-  router.push('/login');
-  return <div>this shipping</div>;
+  const { state, dispatch } = useContext(Store);
+  const { userInfo } = state;
+  useEffect(() => {
+    if (!userInfo) {
+      router.push('/login?redirect=/shipping');
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>This is a shipping</h1>
+    </div>
+  );
 };
 
 export default Shipping;
